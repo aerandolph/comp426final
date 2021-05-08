b0a8e83f8114c2c6e00ee39e64bc45707dd6f982
aerandolph <59979675+aerandolph@users.noreply.github.com>
2021-05-07 22:04:38 -0400

    final login.js

---
+++ login.js
@@ -0,0 +1,157 @@
+export const setupLoginOrCreate = function() {
+    $('#root').append(`
+    <div style="height:100vh" class="has-background-grey">
+    <div class="section has-background-dark">
+        <h1 class="title has-text-grey-lighter">Login Page for Dragon-Fighting Game</h1>
+        <h3 class="subtitle has-text-grey-lighter">made by Alaina Randolph (aer)</h3>
+    </div>
+    <br>
+    <div style="padding:20px" class="section has-background-grey">
+
+        <p class="has-text-grey-lighter">Please log in or make an account to play! :)</p>
+        <br>
+        <button id="loginButton" class="button is-dark" type="button">I already have an account.</button>
+        <button id="createButton" class="button is-dark" type="button">I need to make an account.</button>
+        <button class="button is-dark" onclick="window.location.href='/points_credits.html'">Points and Credits</button>
+        <br>
+        <br>
+        <button class="button is-dark" onclick="window.location.href='/rEnder_it.html'">Bypass Login to Play the Game (Provided for testing purposes)</button>
+    </div>
+    </div>`);
+};
+
+
+export const setupLogin = function(event) {
+    const $root = $('#root');
+    $(event.target.parentNode.parentNode).replaceWith(`<div id="root" style="height:100vh" class="has-background-grey">
+    <div class="section has-background-dark">
+        <h1 class="title has-text-grey-lighter">Login Page for Dragon-Fighting Game</h1>
+        <h3 class="subtitle has-text-grey-lighter">made by Alaina Randolph (aer)</h3>
+    </div>
+    <br>
+    <div style="padding:20px" class="section has-background-grey">
+
+        <p class="has-text-grey-lighter">Please log in or select to make an account instead! :)</p>
+        <br>
+        <input class="input" type="text" id="emailLogin" value="Your email"></input>
+        <input class="input" type="text" id="passwordLogin" value="Your password"></input>
+        <br>
+        <button id="submitLogin" class="button is-dark" type="button">Submit!</button>
+        <button id="createButton" class="button is-dark" type="button">I need to make an account instead.</button>
+        <button class="button is-dark" onclick="window.location.href='/points_credits.html'">Points and Credits</button>
+    </div>
+    </div>`);
+};
+
+export const setupError = function(event) {
+    const $root = $('#root');
+    $(event.target.parentNode.parentNode).replaceWith(`<div id="root" style="height:100vh" class="has-background-grey">
+    <div class="section has-background-dark">
+        <h1 class="title has-text-grey-lighter">Login Page for Dragon-Fighting Game</h1>
+        <h3 class="subtitle has-text-grey-lighter">made by Alaina Randolph (aer)</h3>
+    </div>
+    <br>
+    <div style="padding:20px" class="section has-background-grey">
+
+        <p class="has-text-grey-lighter">There was a problem. :( Please try again. (This is the login page, if you need to make an account select that button.)</p>
+        <br>
+        <input class="input" type="text" id="emailLogin" value="Your email"></input>
+        <input class="input" type="text" id="passwordLogin" value="Your password"></input>
+        <br>
+        <button id="submitLogin" class="button is-dark" type="button">Submit!</button>
+        <button id="createButton" class="button is-dark" type="button">I need to make an account instead.</button>
+        <button class="button is-dark" onclick="window.location.href='/points_credits.html'">Points and Credits</button>
+    </div>
+    </div>`);
+};
+
+export const setupCreate = function(event) {
+    const $root = $('#root');
+    $(event.target.parentNode.parentNode).replaceWith(`<div id="root" style="height:100vh" class="has-background-grey">
+    <div class="section has-background-dark">
+        <h1 class="title has-text-grey-lighter">Login Page for Dragon-Fighting Game</h1>
+        <h3 class="subtitle has-text-grey-lighter">made by Alaina Randolph (aer)</h3>
+    </div>
+    <br>
+    <div style="padding:20px" class="section has-background-grey">
+        <p class="has-text-grey-lighter">Please create an account or select to log in instead! :)</p>
+        <br>
+        <input class="input" type="text" id="emailCreate" value="Your email"></input>
+        <input class="input" type="text" id="passwordCreate" value="Your password"></input>
+        <br>
+        <button id="submitCreate" class="button is-dark" type="button">Make Account!</button>
+        <button id="loginButton" class="button is-dark" type="button">Actually, I already have an account.</button>
+        <button class="button is-dark" onclick="window.location.href='/points_credits.html'">Points and Credits</button>
+    </div>
+    </div>`);
+};
+
+export const successfulLogin = function(event, user) {
+    const $root = $('#root');
+    $(event.target.parentNode.parentNode).replaceWith(`<div id="root" style="height:100vh" class="has-background-grey">
+    <div class="section has-background-dark">
+        <h1 class="title has-text-grey-lighter">Login Page for Dragon-Fighting Game</h1>
+        <h3 class="subtitle has-text-grey-lighter">made by Alaina Randolph (aer)</h3>
+    </div>
+    <br>
+    <div style="padding:20px" class="section has-background-grey">
+        <p class="has-text-grey-lighter">Welcome ${user}! :)</p>
+        <br>
+        <button class="button is-dark" onclick="window.location.href='/rEnder_it.html'">Play the Game!</button>
+        <button id="logout" class="button is-dark" >Logout</button>
+    </div>
+    </div>`);
+};
+
+
+
+export const submitLogin = function(event) {
+    let email = $(`#emailLogin`).val();
+    let password = $(`#passwordLogin`).val();
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            let user = userCredential.user.email;
+            successfulLogin(event, user);
+        })
+        .catch((error) => {
+            setupError(event);
+        });
+};
+
+export const logout = function(event) {
+    firebase.auth().signOut().then(() => {
+        setupLogin(event);
+      }).catch((error) => {
+        setupLogin(event);
+      });
+};
+
+export const submitCreate = function(event) {
+    let email = $(`#emailCreate`).val();
+    let password = $(`#passwordCreate`).val();
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            var user = userCredential.user;
+            successfulLogin(event, user);
+        })
+        .catch((error) => {
+            setupError(event);
+        });
+};
+
+
+export const pageManager = function() {
+    const $root = $('#root');
+    setupLoginOrCreate();
+    
+    $root.on("click", "#loginButton", (event) => setupLogin(event));
+    $root.on("click", "#createButton", (event) => setupCreate(event));
+    $root.on("click", "#submitLogin", (event) => submitLogin(event));
+    $root.on("click", "#submitCreate", (event) => submitCreate(event));
+    $root.on("click", "#logout", (event) => logout(event));
+
+};
+
+$(function() {
+    pageManager();
+});
